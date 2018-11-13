2766c9616ecfe10ce610d016cb67de8973496f72
Dean Blackborough <dean@g3d-development.com>
2018-11-13 14:24:18 +0000

    Added /add-expense GET route
    * Added /add-expense GET route

--- app/Http/Controllers/ExpenseController.php
+++ app/Http/Controllers/ExpenseController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Request\Api;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Config;
 
 class ExpenseController extends BaseController
 {
@@ -11,12 +13,25 @@ class ExpenseController extends BaseController
 
     public function addExpense(Request $request)
     {
+        $sub_categories = Api::getInstance()
+            ->public()
+            //->redirectOnFailure('ErrorController@requestStatus')
+            ->get(
+                Config::get('web.config.api_uri_categories') .
+                '/' . Config::get('web.config.api_category_id_1') .
+                '/sub_categories'
+            );
+
         return view(
             'add-expense',
             [
                 'display_navigation' => $this->display_navigation,
                 'display_add_expense' => false,
-                'nav_active' => 'add-expense'
+                'nav_active' => 'add-expense',
+                'category_id_1' => Config::get('web.config.api_category_id_1'),
+                'category_id_2' => Config::get('web.config.api_category_id_1'),
+                'category_id_3' => Config::get('web.config.api_category_id_1'),
+                'sub_categories' => $sub_categories
             ]
         );
     }
